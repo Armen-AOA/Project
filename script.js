b6862e293dc1edeac267b50397a7d960a8830ca5
Armen <armen.naxdalyan@mail.ru>
2021-03-15 19:40:23 +0300

    example

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
-alert('hello world');
+'use strict';
+const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?','');
+const personalMovieDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false
+};
+const question1 = prompt('Один из последних просмотренных фильмов?','');
+const question2 = +prompt('На сколько оцените его?','');
+const question3 = prompt('Один из последних просмотренных фильмов?','');
+const question4 = +prompt('На сколько оцените его?','');
+personalMovieDB.movies[question1] = question2;
+personalMovieDB.movies[question3] = question4;
+
+console.log(personalMovieDB);
